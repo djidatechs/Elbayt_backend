4d25c44af38b3962453d014817b3b7285ad767c7
DJIDA ISSAM <65438809+djidatechs@users.noreply.github.com>
2023-02-04 20:59:21 +0100

    add sql file

---
+++ elbayt.sql
@@ -0,0 +1,236 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : sam. 04 fév. 2023 à 20:57
+-- Version du serveur : 10.4.24-MariaDB
+-- Version de PHP : 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `elbayt`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `communes`
+--
+
+CREATE TABLE `communes` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `wilaya_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `text` varchar(100) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `adress` varchar(100) DEFAULT NULL,
+  `phone` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `realestate_id` int(11) DEFAULT NULL,
+  `sender_id` int(11) NOT NULL,
+  `recipient_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `photos`
+--
+
+CREATE TABLE `photos` (
+  `id` int(11) NOT NULL,
+  `data` blob DEFAULT NULL,
+  `realestate_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `realestate`
+--
+
+CREATE TABLE `realestate` (
+  `id` int(11) NOT NULL,
+  `category` enum('Sale','Exchange','Rent','Vacation Rent') DEFAULT NULL,
+  `property_type` varchar(100) DEFAULT NULL,
+  `surface` float DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  `contact_phone` varchar(100) DEFAULT NULL,
+  `property_address` varchar(100) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `wilaya_id` int(11) DEFAULT NULL,
+  `commune_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `role` enum('user','admin') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `wilayas`
+--
+
+CREATE TABLE `wilayas` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `communes`
+--
+ALTER TABLE `communes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `wilaya_id` (`wilaya_id`),
+  ADD KEY `ix_communes_id` (`id`);
+
+--
+-- Index pour la table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `realestate_id` (`realestate_id`),
+  ADD KEY `sender_id` (`sender_id`),
+  ADD KEY `recipient_id` (`recipient_id`),
+  ADD KEY `ix_messages_id` (`id`);
+
+--
+-- Index pour la table `photos`
+--
+ALTER TABLE `photos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `realestate_id` (`realestate_id`);
+
+--
+-- Index pour la table `realestate`
+--
+ALTER TABLE `realestate`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `wilaya_id` (`wilaya_id`),
+  ADD KEY `commune_id` (`commune_id`),
+  ADD KEY `ix_realestate_id` (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `wilayas`
+--
+ALTER TABLE `wilayas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ix_wilayas_id` (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `communes`
+--
+ALTER TABLE `communes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `photos`
+--
+ALTER TABLE `photos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `realestate`
+--
+ALTER TABLE `realestate`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `wilayas`
+--
+ALTER TABLE `wilayas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `communes`
+--
+ALTER TABLE `communes`
+  ADD CONSTRAINT `communes_ibfk_1` FOREIGN KEY (`wilaya_id`) REFERENCES `wilayas` (`id`);
+
+--
+-- Contraintes pour la table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`realestate_id`) REFERENCES `realestate` (`id`),
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `messages_ibfk_3` FOREIGN KEY (`recipient_id`) REFERENCES `users` (`id`);
+
+--
+-- Contraintes pour la table `photos`
+--
+ALTER TABLE `photos`
+  ADD CONSTRAINT `photos_ibfk_1` FOREIGN KEY (`realestate_id`) REFERENCES `realestate` (`id`);
+
+--
+-- Contraintes pour la table `realestate`
+--
+ALTER TABLE `realestate`
+  ADD CONSTRAINT `realestate_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `realestate_ibfk_2` FOREIGN KEY (`wilaya_id`) REFERENCES `wilayas` (`id`),
+  ADD CONSTRAINT `realestate_ibfk_3` FOREIGN KEY (`commune_id`) REFERENCES `communes` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
